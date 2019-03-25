5913dc43a4a188349bb283c0a3c533861fa3948c
tetymd <yamada.tetsuya258@gmail.com>
2019-03-25 11:42:32 +0900

    add json parse

--- main.go
+++ main.go
@@ -4,26 +4,70 @@ import (
     "fmt"
     "log"
     "net/http"
-    _ "encoding/json"
+    "encoding/json"
 )
+
+type Fortune struct {
+    Name     string `json:"Name"`
+    Describe string `json:"Describe"`
+    Rarity   int    `json:"Rarity"`
+    Weight   int    `json:"Weight"`
+}
+
+type List struct {
+    ListName string    `json:"ListName"`
+    Data     []Fortune `json:"Data"`
+}
 
 func main() {
     log.Println("server start")
-    http.HandleFunc("/api/v1/get/list", GetList)
+    http.HandleFunc("/api/v1/list", GetList)
     http.HandleFunc("/api/v1/get/random", GetRandom)
     http.ListenAndServe(":8000", nil)
 }
 
 func GetList(w http.ResponseWriter, r *http.Request) {
+    daikiti := Fortune{
+        Name: "大吉",
+        Describe: "a",
+        Rarity: 3,
+        Weight: 10,
+    }
+    tyukiti := Fortune{
+        Name: "中吉",
+        Describe: "a",
+        Rarity: 2,
+        Weight: 30,
+    }
+    shokiti := Fortune{
+        Name: "小吉",
+        Describe: "a",
+        Rarity: 1,
+        Weight: 40,
+    }
+    kyo := Fortune{
+        Name: "凶",
+        Describe: "a",
+        Rarity: 0,
+        Weight: 20,
+    }
+    list := List{
+        ListName: "運勢",
+        Data: []Fortune{
+            daikiti,
+            tyukiti,
+            shokiti,
+            kyo,
+        },
+    }
+
+    d, err := json.Marshal(list)
+    if err != nil {
+        fmt.Println(err)
+    }
+
     w.Header().Set("Content-Type", "application/json")
-    fmt.Fprintln(w, `
-        [
-            { "rarity": "大吉" },
-            { "rarity": "中吉" },
-            { "rarity": "小吉" },
-            { "rarity": "凶" }
-        ]
-    `)
+    fmt.Fprintln(w, string(d))
 }
 
 func GetRandom(w http.ResponseWriter, r *http.Request) {
